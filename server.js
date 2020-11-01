930b922ca5d53526d76926832346810f397f6f4b
pratyu <pratyusha.raghupatruni@gmail.com>
2020-10-31 23:30:00 -0700

    setting up the server

--- server.js
+++ server.js
@@ -1,17 +1,33 @@
 const express = require("express");
+const mongoose = require("mongoose");
+const logger = require("morgan");
+const cors = require("cors");
 const path = require("path");
+require("dotenv").config();
+
 const PORT = process.env.PORT || 3001;
 const app = express();
 
 // Define middleware here
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+
 // Serve up static assets (usually on heroku)
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
 }
 
 // Define API routes here
+// Add routes, both API and view
+//app.use(routes);
+
+// Connect to the Mongo DB
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/oasis-diary",{
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useCreateIndex: true,
+  useFindAndModify: false
+});
 
 // Send every other request to the React app
 // Define any API routes before this runs
