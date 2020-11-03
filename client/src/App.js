c586f4295cd22ea0d68c86e3cc48260b3fc132ed
Robeil Aregawi <65261399+Robeil@users.noreply.github.com>
2020-11-02 22:41:10 -0800

    fixed signup page

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,16 @@
 import React, { Component } from "react";
 import { Route } from 'react-router-dom';
 import LandingPage from './Components/LandingPage';
+import LoginForm from './Components/Login';
+import RegisterForm from './Components/Registration';
 import "./App.css";
 
 function App() {
   return (
     <div>
       <Route path="/" exact component={LandingPage} />
-    
+      <Route path="/login" exact component={LoginForm} />
+      <Route path="/register" exact component={RegisterForm} />
     </div>
   );
 }
