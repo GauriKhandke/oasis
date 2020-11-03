5c2ebc877731f03f35911c5febe17c68029dd6ed
Robeil Aregawi <65261399+Robeil@users.noreply.github.com>
2020-11-02 17:28:13 -0800

    created signup form

--- client/src/App.js
+++ client/src/App.js
@@ -1,17 +1,14 @@
 import React, { Component } from "react";
-import logo from "./logo.svg";
+import { Route } from 'react-router-dom';
+import LandingPage from './Components/LandingPage';
+import RegisterForm from './Components/Registration';
 import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <div className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <h2>Welcome to React</h2>
-      </div>
-      <p className="App-intro">
-        To get started, edit <code>src/App.js</code> and save to reload.
-      </p>
+    <div>
+      <Route path="/" exact component={LandingPage} />
+      <Route path="/register" exact component={RegisterForm} />
     </div>
   );
 }
