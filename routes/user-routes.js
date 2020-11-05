5b7c2d525e1f1dc458f253902cb37f12bb89cec0
pratyu <pratyusha.raghupatruni@gmail.com>
2020-11-05 01:17:51 -0800

    fixed authenticate user route

--- routes/user-routes.js
+++ routes/user-routes.js
@@ -2,6 +2,7 @@ const express = require('express');
 const router = express.Router();
 const userController = require('../controllers/user-controller');
 const auth = require('../middleware/auth');
+const User = require('../models/userModel');
 
 // Register new user
 router.route('/api/signup').post(userController.signupUser);
@@ -13,6 +14,15 @@ router.route('/api/login').post(userController.loginUser);
 router.route('/isValidToken').post(userController.checkValidToken);
 
 //Authenticate user
-router.route('/', auth).get(userController.getAuthenticatedUser);
+router.get('/', auth , async (req, res) => {
+	const user = await User.findById(req.user);
+	console.log("User Id is " + req.user);
+    res.json({
+      id: user._id,
+      firstname: user.firstname,
+      lastname: user.lastname,
+      email: user.email,
+  });
+});
 
 module.exports = router;
