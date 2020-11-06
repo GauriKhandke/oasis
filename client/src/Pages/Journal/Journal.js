6e635f80284a869c9eb4d0061e6055369e5155d1
danybb2020 <daniel.balderas@gmail.com>
2020-11-05 21:01:41 -0800

    Components added

--- client/src/Pages/Journal/Journal.js
+++ client/src/Pages/Journal/Journal.js
@@ -2,6 +2,8 @@ import React, { useContext } from 'react';
 import { Link } from 'react-router-dom'
 import Header from '../../Components/Header';
 import UserContext from '../../Context/UserContext';
+import Calendario from '../../Components/Calendar';
+import { Input, TextArea, FormBtn } from '../../Components/TextField';
 
 export default function Journal() {
 	const { userData } = useContext(UserContext);
@@ -12,12 +14,24 @@ export default function Journal() {
 			{userData.user ? (
 				<div>
 					<h1>Welcome to our Journal Page!!!</h1>
+					<Calendario/>
+					<TextArea/>
+					<FormBtn/>
+					
 				</div>
+
 			) : (
         <>
 				<Link to="/login">Please login</Link>
         </>
+
+
 			)}
 		</div>
 	);
+
+	
+
+
+
 }
