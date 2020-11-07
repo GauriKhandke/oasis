a5ffd46c75fe03931581171708ee69f13f6c4984
pratyu <pratyusha.raghupatruni@gmail.com>
2020-11-06 23:43:04 -0800

    removed navbar and changed logout to button

--- client/src/Pages/Journal/Journal.js
+++ client/src/Pages/Journal/Journal.js
@@ -1,37 +1,28 @@
 import React, { useContext } from 'react';
-import { Link } from 'react-router-dom'
+import { Link } from 'react-router-dom';
 import Header from '../../Components/Header';
 import UserContext from '../../Context/UserContext';
 import Calendario from '../../Components/Calendar';
 import { Input, TextArea, FormBtn } from '../../Components/TextField';
 
 export default function Journal() {
-	const { userData } = useContext(UserContext);
+  const { userData } = useContext(UserContext);
 
-	return (
-		<div>
-			<Header />
-			{userData.user ? (
-				<div>
-					<h1>Welcome to our Journal Page!!!</h1>
-					<Calendario/>
-					<TextArea/>
-					<FormBtn/>
-					
-				</div>
-
-			) : (
+  return (
+    <div>
+      <Header />
+      {userData.user ? (
+        <div>
+          <h3>Welcome to our Journal Page!!!</h3>
+          <Calendario />
+          <TextArea />
+          <FormBtn />
+        </div>
+      ) : (
         <>
-				<Link to="/login">Please login</Link>
+          <Link to="/login">Please login</Link>
         </>
-
-
-			)}
-		</div>
-	);
-
-	
-
-
-
+      )}
+    </div>
+  );
 }
