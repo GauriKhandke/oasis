c7cf154292b7b8d5e7f1f4fec8ec0dced0382c41
Robeil Aregawi <65261399+Robeil@users.noreply.github.com>
2020-11-09 16:54:26 -0800

    chaning background landing img

---
+++ client/src/Components/Pages/Journal/Journal.js
@@ -0,0 +1,31 @@
+import React, { useContext } from 'react';
+import { Link } from 'react-router-dom';
+import Header from '../../Components/Header';
+import UserContext from '../../Context/UserContext';
+import Calendario from '../../Components/Calendar';
+import RecentNotes from '../../Components/RecentNotes';
+import { Input, TextArea, FormBtn } from '../../Components/TextField';
+
+export default function Journal() {
+  const { userData } = useContext(UserContext);
+
+  return (
+    <div>
+      <Header />
+      {userData.user ? (
+        <div>
+          <h3>Welcome to our Journal Page!!!</h3>
+          <Calendario />
+          <TextArea />
+          <FormBtn />
+		  <RecentNotes />
+        </div>
+      ) : (
+        <>
+
+          <Link to="/login">Please login</Link>
+        </>
+      )}
+    </div>
+  );
+}
