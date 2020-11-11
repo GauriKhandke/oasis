0e95efcf4d2796f7a389eeb6b8b1609569e8c60f
Gauri Khandke <khandkegauri@gmail.com>
2020-11-11 02:34:47 -0800

    Added grid and text editor component

--- client/src/Pages/Journal/Journal.js
+++ client/src/Pages/Journal/Journal.js
@@ -1,31 +1,48 @@
 import React, { useContext } from 'react';
+import { Container, Row, Col } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
 import Header from '../../Components/Header';
 import UserContext from '../../Context/UserContext';
 import Calendario from '../../Components/Calendar';
 import RecentNotes from '../../Components/RecentNotes';
 import { Input, TextArea, FormBtn } from '../../Components/TextField';
+import TextEditor from '../../Components/TextEditor';
+import './style.css';
 
 export default function Journal() {
-  const { userData } = useContext(UserContext);
+	const { userData } = useContext(UserContext);
 
-  return (
-    <div>
-      <Header />
-      {userData.user ? (
-        <div>
-          <h3>Welcome to our Journal Page!!!</h3>
-          <Calendario />
-          <TextArea />
-          <FormBtn />
-		  <RecentNotes />
-        </div>
-      ) : (
-        <>
+	return (
+		<>
+			<Header />
+			{userData.user ? (
+				<>
+					<Container fluid="xs">
+						<Row>
+							<Col md={3}>
+								<Calendario />
+							</Col>
 
-          <Link to="/login">Please login</Link>
-        </>
-      )}
-    </div>
-  );
+							<Col md={9}>
+								<Row>
+									<Col md={10}>
+										<TextEditor />
+										{/* <TextArea /> */}
+										{/* <FormBtn /> */}
+										{/* <RecentNotes /> */}
+									</Col>
+								</Row>
+							</Col>
+						</Row>
+					</Container>
+				</>
+			) : (
+				<>
+					<h2 className="text-center">
+						<Link to="/login">Please login</Link>
+					</h2>
+				</>
+			)}
+		</>
+	);
 }
