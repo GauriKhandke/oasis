19c9ca5dd464302f7f658b20592eba07c981f1de
Gauri Khandke <khandkegauri@gmail.com>
2020-11-13 21:20:34 -0800

    Removed upwanted components

--- client/src/Pages/Journal/Journal.js
+++ client/src/Pages/Journal/Journal.js
@@ -3,9 +3,7 @@ import { Container, Row, Col } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
 import Header from '../../Components/Header';
 import UserContext from '../../Context/UserContext';
-import Calendario from '../../Components/Calendar';
 import RecentNotes from '../../Components/RecentNotes';
-import { Input, TextArea, FormBtn } from '../../Components/TextField';
 import TextEditor from '../../Components/TextEditor';
 import './style.css';
 
@@ -20,15 +18,12 @@ export default function Journal() {
 					<Container fluid="xs">
 						<Row>
 							<Col md={2}>
-								{/* <Calendario /> */}
 							</Col>
 
 							<Col md={8}>
 								<Row>
 									<Col md={12}>
 										<TextEditor />
-										{/* <TextArea /> */}
-										{/* <FormBtn /> */}
 										{/* <RecentNotes /> */}
 									</Col>
 								</Row>
