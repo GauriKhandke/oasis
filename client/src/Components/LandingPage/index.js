716b62a133165d0dde99739b00bba32c806fc3e2
Robeil Aregawi <65261399+Robeil@users.noreply.github.com>
2020-11-14 15:06:00 -0800

    added animation, fixed header and background img

--- client/src/Components/LandingPage/index.js
+++ client/src/Components/LandingPage/index.js
@@ -2,34 +2,28 @@ import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 import '../LandingPage/style.css';
 import logo from '../../images/logo.png';
-
+import Animation from './Animation';
 class LandingPage extends Component {
-
-
   render() {
     return (
       <>
-        <nav className="navbar navbar-expand-lg navbar-light bg-light py-2">
-          <div className="navbar-header mx-auto">
-          <img className="logo" src={logo} alt="logo"></img>
-            <h3 className="navbar-brand icon">Oasis</h3>
-          </div>
-          <div className="collapse navbar-collapse mx-auto" id="navbarNav">
-            <div className="nav-link mx-auto">
-              <button className="btn  btn-lg"> {<Link to="/login">Login</Link>}</button>
-             
-              <button className="btn  btn-lg">{<Link to="/contact">Contact</Link>}</button>
+          <nav className="jumbotron position-fixed">
+            <img className="logo" src={logo} alt="logo"></img>
+            <h3 className="position-fixed icon">Oasis</h3>
+            <div>
+              <Link className="btn btn-outline-dark btn-lg float-right" to="/login">Login</Link>
             </div>
-          </div>
-        </nav>
-      
+          </nav>
+    
           <div className="landingImg">
+          <Animation>
             <div className="text-center">
               <h1>Discover Oasis</h1>
               <h2>Keep a Diary, One day it will keep you</h2>
             </div>
+            </Animation>
           </div>
-        
+
       </>
     );
   }
